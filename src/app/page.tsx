db5bad51d20bde030227b602d24cbf87fca5a5e0
Serge <124575375+SergeAtZeDigitalBench@users.noreply.github.com>
2023-12-06 08:57:22 +0000

    feat: homepage, snippets get and view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
-const Home = () => {
-  return <>Home page</>
+import { db } from '@/lib/db'
+
+const HomePage = async () => {
+  const snippets = await db.snippet.findMany()
+
+  return (
+    <>
+      <h1 className="my-4 text-3xl font-bold text-center underline">
+        Home page
+      </h1>
+      <ul className="flex gap-4 justify-center">
+        {(!snippets || snippets.length === 0) && <li>No snippets</li>}
+        {snippets.map(({ id, title, code }) => (
+          <li
+            key={id}
+            className="border border-green-700 bg-green-400 rounded px-2 py-4"
+          >
+            <h3 className="my-2 font-semibold text-xl capitalize">{title}</h3>
+            <code className="p-2 bg-slate-200 rounded">{code}</code>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
-export default Home
+export default HomePage
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -23,8 +23,6 @@ const NewSnippetPage = ({
       },
     })
 
-    console.log('newSnippet :>> ', newSnippet)
-
     redirect('/')
   }
 
